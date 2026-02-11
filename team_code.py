35f7037cde53b5a536f7d81975d550a7095f0b55
WEN Hao <wenh06@gmail.com>
2026-02-12 01:40:01 +0800

    add team_code.py

---
+++ team_code.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# Edit this script to add your team's code. Some functions are *required*, but you can edit most parts of the required functions,
+# change or remove non-required functions, and add your own functions.
+
+################################################################################
+#
+# Optional libraries, functions, and variables. You can change or remove them.
+#
+################################################################################
+
+import os
+
+from helper_code import DEMOGRAPHICS_FILE, find_patients
+
+################################################################################
+# Path & Constant Configuration (Added for Robustness)
+################################################################################
+
+# Get the absolute directory where this script is located
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Build the absolute path to the CSV file relative to the script location
+DEFAULT_CSV_PATH = os.path.join(SCRIPT_DIR, "channel_table.csv")
+
+
+################################################################################
+#
+# Required functions. Edit these functions to add your code, but do not change the arguments for the functions.
+#
+################################################################################
+
+# Train your models. This function is *required*. You should edit this function to add your code, but do *not* change the arguments
+# of this function. If you do not train one of the models, then you can return None for the model.
+
+
+# Train your model.
+def train_model(data_folder, model_folder, verbose, csv_path=DEFAULT_CSV_PATH):
+    # Find the data files.
+    if verbose:
+        print("Finding the Challenge data...")
+
+    patient_data_file = os.path.join(data_folder, DEMOGRAPHICS_FILE)
+    patient_metadata_list = find_patients(patient_data_file)
+    num_records = len(patient_metadata_list)
+
+    if num_records == 0:
+        raise FileNotFoundError("No data were provided.")
+
+    raise NotImplementedError
+
+    # if verbose:
+    #     print('Done.')
+    #     print()
+
+
+# Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function. If you do not train one of the models, then you can return None for the model.
+def load_model(model_folder, verbose):
+    raise NotImplementedError
+
+
+# Run your trained model. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function.
+def run_model(model, record, data_folder, verbose):
+    # Load the model.
+    model = model["model"]
+
+    raise NotImplementedError
+
+    # return binary_output, probability_output
